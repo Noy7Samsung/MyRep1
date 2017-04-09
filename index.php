54f03b6b9f9590211966cdd0f321cda02913562a
Noy7Samsung <noy7.samsung@gmail.com>
2017-04-09 20:13:47 +0700

    t

--- index.php
+++ index.php
@@ -1,3 +1,61 @@
-<?php
-echo "I'm a bot";
-echo "I'm in Thailand";
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>PTT WEB SERVICE</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+
+
+
+<style>
+
+*{margin:0;padding:0;}
+
+                        
+
+</style>
+
+  
+</head>                  
+
+<body>
+       <div style="max-width:100%;width:400px;margin:auto;">
+         <h3>ราคานำ้มันวันนี้</h3>
+    
+         <?php 
+			$client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
+			array(
+				"trace"      => 1,		// enable trace to view what is happening
+				"exceptions" => 0,		// disable exceptions
+				"cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
+			);
+
+			$params = array(
+			   'Language' => "th",
+			   'DD' => date('d'),
+			   'MM' => date('m'),
+			   'YYYY' => date('Y')
+			);
+
+		    $data = $client->GetOilPrice($params);
+			$ob = $data->GetOilPriceResult;
+            $xml = new SimpleXMLElement($ob);
+           
+               // PRICE_DATE , PRODUCT ,PRICE
+            foreach ($xml  as  $key =>$val) {  
+				if($val->PRICE != ''){
+				  echo $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
+				}
+            }
+
+         ?>
+           
+   </div>
+  
+</body>
+
+
+    
+    
+</html>
+    
