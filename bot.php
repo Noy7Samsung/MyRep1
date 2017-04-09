be74a775db41f6fa7b0e3c7a93c1deb2c0ed5ab8
Noy7Samsung <noy7.samsung@gmail.com>
2017-04-09 18:34:08 +0700

    ttt

--- bot.php
+++ bot.php
@@ -18,10 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-			{
 				'type' => 'text',
 				'text' => $text + ', ok na ka'
-			}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
