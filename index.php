a6af96d30e6f48027452c58b7186a625e8f3db75
Noy7Samsung <noy7.samsung@gmail.com>
2017-04-09 20:26:14 +0700

    e
    f

--- index.php
+++ index.php
@@ -24,47 +24,21 @@
     
          <?php 
 		 
-			$curl_h=curl_init();
-curl_setopt($curl_h, CURLOPT_URL, "http://www.pttplc.com/en/GetOilPrice.aspx");
-curl_setopt($curl_h, CURLOPT_RETURNTRANSFER, true);
-$page_c=curl_exec($curl_h);
-curl_close($curl_h);
-
-preg_match('/ALPHA\-X 91.+/sim', $page_c, $extr_1);
-$extr_1=strip_tags($extr_1[0]);
-preg_match('/[1-9][0-9]{0,2}\.[0-9]{0,3}/', $extr_1, $extr_2);
-$extr_2=$extr_2[0]*1;
-echo('PTT ALPHA-X 91ราคา '.$extr_2.' บาท/ลิตร<br>'."\r\n");
-
-preg_match('/GASOHOL.+/sim', $page_c, $extr_1);
-$extr_1=strip_tags($extr_1[0]);
-preg_match('/[1-9][0-9]{0,2}\.[0-9]{0,3}/', $extr_1, $extr_2);
-$extr_2=$extr_2[0]*1;
-echo('PTT GASOHOL 95 ราคา '.$extr_2.' บาท/ลิตร<br>'."\r\n");
-
-preg_match('/GASOHOL 91.+/sim', $page_c, $extr_1);
-$extr_1=strip_tags($extr_1[0]);
-preg_match('/[1-9][0-9]{0,2}\.[0-9]{0,3}/', $extr_1, $extr_2);
-$extr_2=$extr_2[0]*1;
-echo('PTT GASOHOL 91 ราคา '.$extr_2.' บาท/ลิตร<br>'."\r\n");
-
-preg_match('/E20 PLUS.+/sim', $page_c, $extr_1);
-$extr_1=strip_tags($extr_1[0]);
-preg_match('/[1-9][0-9]{0,2}\.[0-9]{0,3}/', $extr_1, $extr_2);
-$extr_2=$extr_2[0]*1;
-echo('PTT E20 PLUS ราคา '.$extr_2.' บาท/ลิตร<br>'."\r\n");
-
-preg_match('/E85 PLUS.+/sim', $page_c, $extr_1);
-$extr_1=strip_tags($extr_1[0]);
-preg_match('/[1-9][0-9]{0,2}\.[0-9]{0,3}/', $extr_1, $extr_2);
-$extr_2=$extr_2[0]*1;
-echo('PTT E85 PLUS ราคา '.$extr_2.' บาท/ลิตร<br>'."\r\n");
-
-preg_match('/DELTA\-X.+/sim', $page_c, $extr_1);
-$extr_1=strip_tags($extr_1[0]);
-preg_match('/[1-9][0-9]{0,2}\.[0-9]{0,3}/', $extr_1, $extr_2);
-$extr_2=$extr_2[0]*1;
-echo('PTT DELTA-X ราคา '.$extr_2.' บาท/ลิตร<br>'."\r\n");
+			$wsdl = 'http://www.pttplc.com/webservice/pttinfo.asmx?wsdl';
+	
+	$client = new SoapClient($wsdl); 
+	
+	$methodName = 'CurrentOilPrice'; 
+	
+	$params = array('Language'=>'EN'); 
+	
+	$soapAction = 'http://www.pttplc.com/ptt_webservice/CurrentOilPrice'; 
+	
+	$objectResult = $client->__soapCall($methodName, array('parameters' => $params), array('soapaction' => $soapAction)); 
+	
+	echo $objectResult->CurrentOilPriceResult;
+	echo '<pre>'.$objectResult->CurrentOilPriceResult.'</pre>';
+	
          ?>
            
    </div>
