0a4ce1a9d97046f558a7517e310a0dbd1ffcd8e4
Noy7Samsung <noy7.samsung@gmail.com>
2017-04-09 22:15:54 +0700

    sdfdsf

--- bot.php
+++ bot.php
@@ -16,12 +16,12 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			if ($text == "image")
+			if ($text == "sticker")
 			{
 				$messages = [
-					'type' => 'image',
-					'originalContenUrl' => 'https://scontent-fbkk5-7.us-fbcdn.net/v1/t.1-48/1426l78O9684I4108ZPH0J4S8_842023153_K1DlXQOI5DHP/dskvvc.qpjhg.xmwo/p/data/139/139491-7-5535.jpg',
-					'previewImageUrl' => 'https://scontent-fbkk5-7.us-fbcdn.net/v1/t.1-48/1426l78O9684I4108ZPH0J4S8_842023153_K1DlXQOI5DHP/dskvvc.qpjhg.xmwo/p/data/139/139491-7-5535.jpg'
+					'type' => 'sticker',
+					'packageId' => '1'
+					'stickerId' => '16'
 				];
 			}else{
 				$messages = [
